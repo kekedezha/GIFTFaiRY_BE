e6cf6089092563be917db5ee39312468c4ed44cf
TheoThibodeau <teddythibodeau@gmail.com>
2023-11-13 16:34:52 -0500

    made migrations on the activity level

--- bestow/models.py
+++ bestow/models.py
@@ -21,6 +21,7 @@ class Filter(models.Model):
     occasion = models.CharField(max_length=300)
     gift_type = models.CharField(max_length=300)
     interest = models.CharField(max_length=300)
+    activity_level = models.CharField(max_length=300)
     user = models.ForeignKey(
         to=User, on_delete=models.CASCADE, related_name="filters", blank=True, null=True)
     output_text = models.TextField(default='')
@@ -95,7 +96,7 @@ class Filter(models.Model):
     def send_filters(self):
         # Load environment variables from a .env file in the current directory
         load_dotenv()
-        filters_input = f'You are a tool that helps people buy gifts for others. Suggest 10 ideal gifts for somebody with these characteristics and traits, they are {self.age} years old, their gender identify is {self.gender}, they are my {self.relationship}, my price range is {self.price_range}, the occasion this gift is for is {self.occasion}, I want to give them a {self.gift_type}, their main interest is {self.interest}. Please take all parameters into equal consideration.'
+        filters_input = f'You are a tool that helps people buy gifts for others. Suggest 10 ideal gifts for somebody with these characteristics and traits, they are {self.age} years old, their gender identify is {self.gender}, they are my {self.relationship}, my price range is {self.price_range}, the occasion this gift is for is {self.occasion}, I want to give them a {self.gift_type}, their main interest is {self.interest}, and their activity level is {self.activity_level}. Please take all parameters into equal consideration.'
 
         MODEL = "gpt-3.5-turbo"
         openai.api_key = os.getenv('OPENAI_API_KEY')
