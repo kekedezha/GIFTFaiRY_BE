03f21f15617c98515cf265b9c958c83085507dd5
TheoThibodeau <teddythibodeau@gmail.com>
2023-11-20 17:43:38 -0500

    ehhH

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-from corsheaders.decorators import cors_headers
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
