2a4fbe603dc2e5bfd0827e87eb0e743c8ff2a9c6
TheoThibodeau <teddythibodeau@gmail.com>
2023-11-28 16:27:27 -0500

    added env to settings.py

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,17 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import environ
 
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False),
+    RENDER=(bool, False),
+)
+
+
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
