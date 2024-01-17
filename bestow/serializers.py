4b71f1622bb4595c4be2bd69775c74905b45e30d
TheoThibodeau <teddythibodeau@gmail.com>
2024-01-17 15:48:24 -0500

    added all of the necessary fields to serializers.py

--- bestow/serializers.py
+++ bestow/serializers.py
@@ -20,4 +20,17 @@ class FilterPostSerializer(serializers.ModelSerializer):
 class FilterGetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Filter
-        fields = "__all__"
+        fields = [
+            "age",
+            "gender",
+            "relationship",
+            "price_range",
+            "occasion",
+            "gift_type",
+            "interest",
+            "activity_level",
+            "personality",
+            "nature",
+            "created_at",
+            "item_title_string",
+        ]
