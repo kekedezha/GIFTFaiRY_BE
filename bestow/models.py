ac2bd7a63948fe1fd44de66024afa2c7d4b94421
kekedezha <dezhac6@gmail.com>
2024-01-19 17:30:26 -0500

    edited join on line 96 in models.py

--- bestow/models.py
+++ bestow/models.py
@@ -93,7 +93,7 @@ class Filter(models.Model):
                 self.item_title_array)-1] = self.item_title_array[len(self.item_title_array)-1].lstrip(" ")
 
         # Join all elements of parsed arrays into separate strings to have ready to pass over to front end.
-        self.item_title_string = " ,".join(self.item_title_array)
+        self.item_title_string = ", ".join(self.item_title_array)
         # The * seperates each description, and allows us to parse each description to its own response card
         self.item_descrip_string = "*".join(self.item_descrip_array)
         self.openai_descrip_string = ",".join(self.openai_descrip_array)
