8cab8ee0706821cfc61a1f7de271a3689012f052
TheoThibodeau <teddythibodeau@gmail.com>
2024-02-07 10:19:27 -0500

    defined email in filtergetviewset
    Co-authored-by: Christian Dezha <dezhac6@gmail.com>

--- bestow/views.py
+++ bestow/views.py
@@ -35,9 +35,19 @@ class FilterGetViewSet(generics.ListAPIView):
     '''
     METHODS: GET
     '''
-    queryset = Filter.objects.filter(username=email)
+
     serializer_class = FilterGetSerializer
     lookup_field = "email"
+
+    def get_queryset(self):
+        # Get the email of the currently authenticated user
+        email = self.request.user.email
+        # Filter queryset based on the user's email address
+        return Filter.objects.filter(email=email)
+    # queryset = Filter.objects.filter(email=email)
+    # serializer_class = FilterGetSerializer
+    # lookup_field = "email"
+    
 
     # def get_queryset(self):
     #     # Filter queryset based on the currently authenticated user's email address
