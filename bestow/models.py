ae4cc0c649dd38437c2e3e6325c261d438a6a94d
TheoThibodeau <teddythibodeau@gmail.com>
2024-03-07 17:10:53 -0500

    added self.item_title_string to the clearAsterisk function
    Co-authored-by: Christian Dezha <dezhac6@gmail.com>

--- bestow/models.py
+++ bestow/models.py
@@ -145,9 +145,9 @@ class Filter(models.Model):
 
     def clearAsterisk(self, string):
         if string.count("**") >= 1:
-            string=string.replace("**", "")
+            self.item_title_string=string.replace("**", "")
         elif string.count("*") >= 1:
-            string=string.replace("*", "")
+            self.item_title_string=string.replace("*", "")
 
 
     def send_filters(self):
