49ec0b1d6bd328d90e33e43d84bb87acb23d7ee5
kekedezha <dezhac6@gmail.com>
2024-03-08 11:19:03 -0500

    Updated parsing function for list of items only and f string for OpenAI API call
    Co-authored-by: Theodore Thibodeau <teddythibodeau@gmail.com>

--- bestow/models.py
+++ bestow/models.py
@@ -62,7 +62,8 @@ class Filter(models.Model):
         userInstance = User.objects.get(email=self.email)
         return userInstance
 
-    def parseIfBeginWithNum(self, string):
+    # Function to parse OpenAIs response if response is a numbered list.
+    def numberedListParsing(self, openAIResponseString):
         # Clears string and arrays each time we send a POST and GET a request back from OpenAI
         self.item_descrip_string = ""
         self.item_title_string = ""
@@ -71,7 +72,20 @@ class Filter(models.Model):
         self.item_title_array = []
         self.openai_descrip_array = []
 
-        for x in string:
+        # For the incoming string, separate the string into an array to start parsing.
+        if (openAIResponseString.count("\n\n") >= 3):
+            parsedArray = str(openAIResponseString).split("\n\n")
+        else:
+            parsedArray = str(openAIResponseString).split("\n")
+        # Create array that will hold the string with the list of items after the numerical value has been cleared
+        items_and_descrip_Array = []
+
+        # Parse array one extra step to get rid of item number
+        for items in parsedArray:
+            updatedString = items[3:]
+            items_and_descrip_Array.append(updatedString)
+
+        for x in items_and_descrip_Array:
             indexAt = x.find(":")
             # This if will catch the ValueError: substring not found. Arrays are parsed correctly but when items are separated by only one '\n' then
             # extra index at the front of the array and at the last element. They are empty strings and do not contain ":". Since the .find() will return a -1
@@ -87,11 +101,13 @@ class Filter(models.Model):
 
         # Join all elements of parsed arrays into separate strings to have ready to pass over to front end.
         self.item_title_string = ", ".join(self.item_title_array)
-        # The * seperates each description, and allows us to parse each description to its own response card
+        # The * separates each description, and allows us to parse each description to its own response card
         self.item_descrip_string = "*".join(self.item_descrip_array)
         self.openai_descrip_string = ",".join(self.openai_descrip_array)
 
-    def parseIfBeginNormal(self, string):
+    # Function to parse OpenAIs response if response has extra content that is unnecessary for the user.
+    # Function may not be used anymore as of 3/8/2024. Updated API call to specify response back from OpenAI
+    def parseIfBeginNormal(self, openAIResponseString):
         # Clears string and arrays each time we send a POST and GET a request back from OpenAI
         self.item_descrip_string = ""
         self.item_title_string = ""
@@ -101,10 +117,10 @@ class Filter(models.Model):
         self.openai_descrip_array = []
 
         # Parsed output_text response to initial array that will be used for further parsing
-        if (string.count("\n\n") >= 3):
-            parsedArray = str(string).split("\n\n")
+        if (openAIResponseString.count("\n\n") >= 3):
+            parsedArray = str(openAIResponseString).split("\n\n")
         else:
-            parsedArray = str(string).split("\n")
+            parsedArray = str(openAIResponseString).split("\n")
         # Parsed array length
         parsedArrayLen = len(parsedArray)
         items_and_descrip_Array = []
@@ -113,7 +129,7 @@ class Filter(models.Model):
         self.openai_descrip_array.append(parsedArray[0])
         self.openai_descrip_array.append(parsedArray[parsedArrayLen-1])
 
-        # Get rid of first and last item of intial array that contains openAI general description of responses already stored in separate array above
+        # Get rid of first and last item of initial array that contains openAI general description of responses already stored in separate array above
         parsedArray.pop(0)
         parsedArray.pop(len(parsedArray)-1)
 
@@ -145,15 +161,16 @@ class Filter(models.Model):
 
     def clearAsterisk(self, string):
         if string.count("**") >= 1:
-            self.item_title_string=string.replace("**", "")
+            self.item_title_string = string.replace("**", "")
         elif string.count("*") >= 1:
-            self.item_title_string=string.replace("*", "")
-
+            self.item_title_string = string.replace("*", "")
 
     def send_filters(self):
         # Load environment variables from a .env file in the current directory
         load_dotenv()
-        filters_input = f'Consider the following details about the recipient: they are a {self.age}, identify as {self.gender}, they are my {self.relationship}, my price range is {self.price_range}, the occasion this gift is for is {self.occasion}, I want to give them a {self.gift_type}, their main interest is {self.interest}, and their activity level is {self.activity_level}. They are {self.personality} and they prefer being {self.nature}. Please provide a curated list of 10 diverse and thoughtful gift suggestions that take into account all the specified characteristics. Please cater the gift suggestions to retail products, do not suggest live events or anything else that is not a physical product. Lastly, please separate all gift ideas and their desciptions by a colon (:) and not by a dash (-).'
+        # Updated end of string to specify a list of ideas.
+        # -Christian Dezha 3/8/2024
+        filters_input = f'Consider the following details about the recipient: they are a {self.age}, identify as {self.gender}, they are my {self.relationship}, my price range is {self.price_range}, the occasion this gift is for is {self.occasion}, I want to give them a {self.gift_type}, their main interest is {self.interest}, and their activity level is {self.activity_level}. They are {self.personality} and they prefer being {self.nature}. Please provide a curated list of 10 diverse and thoughtful gift suggestions that take into account all the specified characteristics. Please cater the gift suggestions to retail products, do not suggest live events or anything else that is not a physical product. Lastly, please make sure to list all gift ideas separate them and their descriptions by a colon (:) and not by a dash (-).'
 
         MODEL = "gpt-3.5-turbo"
 
@@ -172,7 +189,7 @@ class Filter(models.Model):
         # if the response we get back from OpenAI beings with "1. ... 2..." then parse with the following method
         if (self.output_text[0] == '1'):
             print("Parsing with 'parseIfBeginWithNum' method")
-            self.parseIfBeginWithNum(self.output_text)
+            self.numberedListParsing(self.output_text)
         # Else if the response we get back from OpenAI beings with "Based on..." or some other text then parse normally
         else:
             print("Parsing normally")
