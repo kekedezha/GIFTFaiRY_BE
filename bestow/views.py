8dddde184e3ed99401c5ced742ec9ce0febec5b4
kekedezha <dezhac6@gmail.com>
2025-09-30 17:15:00 -0400

    updated view for retrieving user to include proper status code

--- bestow/views.py
+++ bestow/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from .models import User, Filter
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
 from .serializers import UserPostSerializer, FilterPostSerializer, FilterGetSerializer, UserGetSerializer
-from django.http import HttpResponse
 # Create your views here.
 
 
@@ -23,21 +24,24 @@ class UserPostViewSet(generics.CreateAPIView):
         user = serializer.save()
         user.saveToUserDatabase()
 
-class UserGetViewSet(generics.ListAPIView):
+class UserGetViewSet(generics.RetrieveAPIView):
     '''
     METHODS: GET
+    Retrieves a single user by email. Returns 404 if not found. 
     '''
-    # Save data from User table specified in the UserGetSerialzer class
-    # to the serializer_class variable
-    serializer_class = UserGetSerializer
-
-    # Overwrites getqueryset function which is a part of GetViewSet class
+    
+    serializer_class = UserGetSerializer # Save data from User table specified in the UserGetSerializer class to the serializer_class variable
+    lookup_field = 'email' # Tells Django to look for 'email' field in the URL
     def get_queryset(self):
-        # Get the user ID from the URL parameters
-        email = self.kwargs['email']
-        # User queryset based on the user ID
-        return User.objects.filter(email=email)
+        return User.objects.all() # Queryset to retrieve all User objects from the database
 
+    def get_object(self):
+        queryset = self.get_queryset() # Get the queryset
+        email = self.kwargs.get(self.lookup_field) # Get the email from the URL parameters
+        try:
+            return queryset.get(email=email) # Return the serialized data
+        except User.DoesNotExist:
+            raise NotFound(detail="User not found") # Return 404 if user not found
 
 class FilterPostViewSet(generics.CreateAPIView):
     '''
@@ -55,11 +59,11 @@ class FilterGetViewSet(generics.ListAPIView):
     '''
     METHODS: GET
     '''
-    # Save data from Filter table specified in the FilterGetSerialzer class
+    # Save data from Filter table specified in the FilterGetSerializer class
     # to the serializer_class variable
     serializer_class = FilterGetSerializer
 
-    # Overwrites getqueryset function which is a part of GetViewSet class
+    # Overwrites get_queryset function which is a part of GetViewSet class
     def get_queryset(self):
         # Get the user ID from the URL parameters
         email = self.kwargs['email']
